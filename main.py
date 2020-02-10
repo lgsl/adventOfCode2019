1a453f1bd3d18ebd27e3296cc5e8f0a941f0e0a2
Lars Lundqvist <lars_lundqvist@hotmail.com>
2020-02-10 20:28:36 +0100

    Day 1 fully complete

--- main.py
+++ main.py
@@ -1,24 +1,22 @@
 import math
 
-
+# Part 1
 # Calculates the required fuel to launch the given module
 def fuel_calculator(mass):
     return math.floor((mass / 3)) - 2
-
 
 puzzleInput = open("input.txt", "r")
 fuel_requirements = [fuel_calculator(int(mass)) for mass in puzzleInput]
 modules_fuel_requirement = sum(fuel_requirements)
 print(modules_fuel_requirement)
 
-total_fuel_counter = 33583
-new_fuel_requirement = fuel_calculator(33583)
-#total_fuel_counter += new_fuel_requirement
+# Part 2
+def fuel_of_fuel(fuel_mass):
+    counter = 0
+    new_fuel_requirement = fuel_calculator(fuel_mass)
+    while new_fuel_requirement > 0:
+        counter += new_fuel_requirement
+        new_fuel_requirement = fuel_calculator(new_fuel_requirement)
+    return counter
 
-while new_fuel_requirement > 1:
-    print("fuel for fuel " + str(new_fuel_requirement))
-    total_fuel_counter += new_fuel_requirement
-    new_fuel_requirement = fuel_calculator(new_fuel_requirement)
-    #total_fuel_counter += new_fuel_requirement
-
-print(total_fuel_counter)
+print(sum([fuel_of_fuel(x) for x in fuel_requirements]) + modules_fuel_requirement)
